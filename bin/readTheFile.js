32c5291ed9bd4686b22c01e4547731e921dee9db
siemchic <siemchic@mail.ru>
2024-04-15 21:42:09 +0500

    testPUSH

--- bin/readTheFile.js
+++ bin/readTheFile.js
@@ -1,5 +1,7 @@
 import * as path from 'path';
 import * as fs from 'fs';
+// eslint-disable-next-line import/no-unresolved
+import _ from 'lodash';
 
 const readthefile = (filepath1, filepath2) => {
   const rezult = path.resolve(filepath1);
@@ -8,5 +10,6 @@ const readthefile = (filepath1, filepath2) => {
   const fileContent2 = fs.readFileSync(rezult2, 'utf-8');
   console.log(fileContent);
   console.log(fileContent2);
+  console.log(JSON.stringify(fileContent));
 };
 export default readthefile;
